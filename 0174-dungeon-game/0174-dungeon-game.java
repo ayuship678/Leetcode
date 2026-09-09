2ab4e328894c001371ad28885343266e3e5e0d8d
ayuship678 <paulayushi71@gmail.com>
2026-09-09 10:35:19 +0530

    Time: 2 ms (68.79%), Space: 45.5 MB (21.38%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int n = dungeon.length;
+        int m = dungeon[0].length;
+        int[][] dp = new int[n][m];
+        dp[n-1][m-1] = Math.max(1,1-dungeon[n-1][m-1]);
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i][m-1] = Math.max(1,dp[i+1][m-1]-dungeon[i][m-1]);
+        }
+        for (int j = m - 2; j >= 0; j--) {
+            dp[n-1][j] = Math.max(1,dp[n-1][j+1]-dungeon[n-1][j]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = m - 2; j >= 0; j--) {
+                dp[i][j] = Math.max(1,Math.min(dp[i+1][j],dp[i][j+1]) - dungeon[i][j]);
+            }
+        }
+        return dp[0][0];
+    }
+}
